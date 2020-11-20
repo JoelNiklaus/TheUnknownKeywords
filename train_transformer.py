2c51376894c178cee27a67595e80caea739f5bb8
Joel Niklaus <me@joelniklaus.ch>
2020-11-20 18:03:56 +0100

    dropped Unnamed column and saved files to json additionally

--- train_transformer.py
+++ train_transformer.py
@@ -19,16 +19,20 @@ def train_transformer_pipeline(data_dir, transformers=[]):
     train_set.drop(columns=['Impact', 'Urgency', 'IncidentType', 'ServiceProcessed', 'ManualGroups'], inplace=True)
     train_set['ServiceProcessed'] = labels['ServiceProcessed']
     train_set['ManualGroups'] = labels['ManualGroups']
-    train_set.rename(columns={'Unnamed: 0': ''}, inplace=True)
+    train_set.drop(columns=['Unnamed: 0'], inplace=True)
 
     train_csv, validation_trans = train_test_split(train_set, test_size=val_size)
     train_csv.to_csv(data_dir / 'train_trans.csv', sep=';', index=False)
+    train_csv.to_json(data_dir / 'train_trans.json', orient='table')
+
     validation_trans.to_csv(data_dir / 'validation_trans.csv', sep=';', index=False)
+    validation_trans.to_json(data_dir / 'validation_trans.json', orient='table')
 
     test_set = pd.read_csv(data_dir / 'test_reduced.csv', delimiter=';')
     if len(transfomers > 0):
         apply_transformers(test_set, transformers)
     test_set['ServiceProcessed'] = ' '
     test_set['ManualGroups'] = ' '
-    test_set.rename(columns={'Unnamed: 0': ''}, inplace=True)
-    test_set.to_csv(data_dir / 'test_trans.csv', index=False, sep=';')
+    test_set.drop(columns=['Unnamed: 0'], inplace=True)
+    test_set.to_csv(data_dir / 'test_trans.csv', sep=';', index=False)
+    test_set.to_json(data_dir / 'test_trans.json', orient='table')
