162281d5d87a5e125a5e9393d07c5cc302ea1e3d
Dominic Schweizer <dominic.schweizer@inf.unibe.ch>
2020-11-20 16:23:49 +0100

    added testTransformer

--- train_transformer.py
+++ train_transformer.py
@@ -7,4 +7,8 @@ def train_transformer_pipeline():
     train_set['ManualGroups'] = labels['ManualGroups']
     train_set.rename(columns={'Unnamed: 0':''}, inplace=True)
     train_set.to_csv('train_trans.csv', sep=';', index=False)
+    test_set = pd.read_csv('test_reduced.csv', delimiter=';')
+    test_set['ServiceProcessed'] = ''
+    test_set['ManualGroups'] = ''
+    test_set.to_csv('test_trans.csv', index=False, sep=';')
 train_transformer_pipeline()
