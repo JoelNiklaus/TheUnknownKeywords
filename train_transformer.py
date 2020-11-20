c2d0b51870638b91948b1afe2649289a173a5736
Dominic Schweizer <dominic.schweizer@inf.unibe.ch>
2020-11-20 16:46:56 +0100

    added val set

--- train_transformer.py
+++ train_transformer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from bert import DATASET_DIR
-
+test_size = 0.3
 
 def train_transformer_pipeline():
     train_set = pd.read_csv(DATASET_DIR / 'train.csv', delimiter=';')
@@ -11,7 +11,9 @@ def train_transformer_pipeline():
     train_set['ManualGroups'] = labels['ManualGroups']
     train_set.rename(columns={'Unnamed: 0': ''}, inplace=True)
     train_set.to_csv(DATASET_DIR / 'train_trans.csv', sep=';', index=False)
-
+    train_csv, val_csv = train_test_split(train_set, test_size=test_size)
+    train_csv.to_csv(DATASET_DIR / 'train_trans.csv', sep=';', index=False)
+    val_csv.to_csv(DATASET_DIR / 'val_trans.csv', sep=';', index=False)
     test_set = pd.read_csv(DATASET_DIR / 'test_reduced.csv', delimiter=';')
     test_set['ServiceProcessed'] = ' '
     test_set['ManualGroups'] = ' '
